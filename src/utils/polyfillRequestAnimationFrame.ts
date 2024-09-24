567d671e9b80f03b957c0f6f9dc1c9091a8298cf
Pearce <pearce@sigmacomputing.com>
2024-09-24 15:40:43 -0700

    Various build fixes and formatting

--- src/utils/polyfillRequestAnimationFrame.ts
+++ src/utils/polyfillRequestAnimationFrame.ts
@@ -1,6 +1,6 @@
 /**
  * requestAnimationFrame() calls are paused in most browsers when running in background tabs or hidden <iframe>s in order to improve performance and battery life
- * 
+ *
  * @see https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
  * @param w Window Object
  */
