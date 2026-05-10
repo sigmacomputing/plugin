fb0c4acccda23db453d7cf033ee8e5aa3ab48261
Pearce Ropion <pearce@sigmacomputing.com>
2026-05-10 10:57:42 -0700

    Fix nested build and test configs

---
+++ packages/plugin-sdk/tsdown.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig, mergeConfig } from 'tsdown';
+
+// @ts-ignore - base config is defined outside of this package
+import baseConfig from '../../tsdown.base.ts';
+
+import packageJson from './package.json' with { type: 'json' };
+
+export default mergeConfig(
+  baseConfig,
+  defineConfig({
+    define: {
+      __VERSION__: JSON.stringify(packageJson.version),
+    },
+  }),
+);
